11376f66f11b5cc621c9de756f0352eaf18f4b28
vursha <97270930+vursha@users.noreply.github.com>
2022-01-07 10:21:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function add(a,b){
+	return a+b;
+}
+
+function welcomeUser(name){
+alert('welcome '+name+" !");
+}
+
+export {add,welcomeUser};
---
+++ index.js
@@ -0,0 +1,6 @@
+import {add} from "./app.js";
+import { welcomeUser} from "./app.js";
+var res=add(1,2);
+alert(res);
+
+welcomeUser("nitin");
